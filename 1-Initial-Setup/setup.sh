14fafe6bb3c7c1f938b392f40f6697b2abb76096
goldenpassport-admin <support@goldenpassport.com>
2019-04-07 19:16:07 +1000

    Added Metal and Traefik

---
+++ 1-Initial-Setup/setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#
+# Configuration
+#
+if [ -z "${KUBECONFIG}" ]; then
+    export KUBECONFIG=~/.kube/config
+fi
+
+# CAUTION - setting NAMESPACE will deploy most components to the given namespace
+# however some are hardcoded to 'monitoring'. Only use if you have reviewed all manifests.
+
+if [ -z "${NAMESPACE}" ]; then
+    NAMESPACE=kube-system
+fi
+
+kctl() {
+    kubectl --namespace "$NAMESPACE" "$@"
+}
+
+#
+# Background Complete
+#
+
+
+#
+# Setup (A - C)
+#
+
+# A. Core Configmaps
+kctl apply -f a-core-configmaps.yaml
+
+# B. MetalLB
+kctl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+
+# C. Traefik (Internal)
+kctl apply -f traefik-rbac.yaml
+
+kctl apply -f traefik-internal-service.yaml
+kctl apply -f traefik-internal-deployment.yaml
+
+
+
+
