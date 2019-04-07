5d3c158c368e7f57adf01e712de8e565de66fc23
goldenpassport-admin <support@goldenpassport.com>
2019-04-07 19:21:05 +1000

    Added Metal and Traefik

--- 1-Initial-Setup/setup.sh
+++ 1-Initial-Setup/setup.sh
@@ -31,13 +31,13 @@ kctl() {
 kctl apply -f a-core-configmaps.yaml
 
 # B. MetalLB
-kctl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
 
 # C. Traefik (Internal)
-kctl apply -f traefik-rbac.yaml
+kctl apply -f c-traefik-rbac.yaml
 
-kctl apply -f traefik-internal-service.yaml
-kctl apply -f traefik-internal-deployment.yaml
+kctl apply -f c-traefik-internal-service.yaml
+kctl apply -f c-traefik-internal-deployment.yaml
 
 
 
