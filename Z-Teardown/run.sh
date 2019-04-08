c0d10132e94f50501afa8ea8ee50d3354af4a663
goldenpassport-admin <support@goldenpassport.com>
2019-04-08 16:51:56 +1000

    Initial

--- Z-Teardown/run.sh
+++ Z-Teardown/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl drain rock1 --delete-local-data --force --ignore-daemonsets
