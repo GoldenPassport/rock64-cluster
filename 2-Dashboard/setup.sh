31117e6249ce5c648854e87278a802847bfe6bb2
goldenpassport-admin <support@goldenpassport.com>
2019-04-08 13:18:49 +1000

    Dashbaords

---
+++ 2-Dashboard/setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#
+# Configuration
+#
+if [ -z "${KUBECONFIG}" ]; then
+    export KUBECONFIG=~/.kube/config
+fi
+
+# CAUTION - setting NAMESPACE will deploy most components to the given namespace
+# however some are hardcoded to 'monitoring'. Only use if you have reviewed all manifests.
+
+if [ -z "${NAMESPACE}" ]; then
+    NAMESPACE=kube-system
+fi
+
+kctl() {
+    kubectl --namespace "$NAMESPACE" "$@"
+}
+
+#
+# Background Complete
+#
+
+
+#
+# Setup 
+#
+
+kctl apply -f .
+
+
+
+
