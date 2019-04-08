30f8206e5b72859796e8964a713b14d953dab87e
goldenpassport-admin <support@goldenpassport.com>
2019-04-08 14:48:59 +1000

    Initial

---
+++ 1-Install/run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+echo "Container Linux installation script"
+echo "This will install:"
+echo " - Docker Community Edition"
+echo " - Docker Compose"
+echo " - Kubernetes: kubeadm, kubelet and kubectl"
+echo ""
+
+set -xeo pipefail
+
+#
+# A. Install Docker CE
+#
+
+## Set up the repository:
+### Install packages to allow apt to use a repository over HTTPS
+apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+### Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+### Add Docker apt repository.
+sudo add-apt-repository \
+   "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+## Install Docker CE.
+apt-get update && apt-get install docker-ce docker-compose 
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+#
+# B. Install Kubernetes
+#
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update -y
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+# Add User to Docker Group
+usermod -aG docker $USER
+
+# Init Kubernetes
+kubeadm config images pull
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+#
+# C. Post Install Setup
+#
+
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+su rock
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Install Flannel
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
+
+# Update Packages
+apt-get update
+apt-get upgrade
---
+++ Z-Teardown/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+kubectl drain rock1 --delete-local-data --force --ignore-daemonsets
+kubectl delete node rock1
+kubectl drain rock2 --delete-local-data --force --ignore-daemonsets
+kubectl delete node rock2
+kubectl drain rock3 --delete-local-data --force --ignore-daemonsets
+kubectl delete node rock3
+kubectl drain rock4 --delete-local-data --force --ignore-daemonsets
+kubectl delete node rock4
+
+kubeadm reset
+
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
