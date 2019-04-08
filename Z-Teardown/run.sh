4f4cac36921afd3090a8d12492b85ec7c9ac9f91
goldenpassport-admin <support@goldenpassport.com>
2019-04-08 16:41:54 +1000

    Initial

--- Z-Teardown/run.sh
+++ Z-Teardown/run.sh
@@ -9,6 +9,7 @@ kubectl delete node rock3
 kubectl drain rock4 --delete-local-data --force --ignore-daemonsets
 kubectl delete node rock4
 
+echo "Root password"
 su root
 kubeadm reset -f
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
