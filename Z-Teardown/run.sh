3a86e7a48f4e071ae416a8734e4b232ce2cbc464
goldenpassport-admin <support@goldenpassport.com>
2019-04-08 18:39:39 +1000

    Initial

--- Z-Teardown/run.sh
+++ Z-Teardown/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-echo "### Need to be logged in as root"
+echo "####################################"
+echo "### Need to be logged in as root ###"
+echo "####################################"
 
 # su - rock -c "mkdir -p $HOME/.kube"
 # su - rock -c "sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config"
 # su - rock -c "sudo chown $(id -u):$(id -g) $HOME/.kube/config"
+
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
 su - rock -c "kubectl drain rock1 --delete-local-data --force --ignore-daemonsets"
 su - rock -c "kubectl delete node rock1"
@@ -16,4 +20,3 @@ su - rock -c "kubectl drain rock4 --delete-local-data --force --ignore-daemonset
 su - rock -c "kubectl delete node rock4"
 
 kubeadm reset -f
-iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
