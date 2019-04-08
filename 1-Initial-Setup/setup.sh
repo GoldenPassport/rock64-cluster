ce51b393c2797824edc7119a8b2f90933c1d3e00
goldenpassport-admin <support@goldenpassport.com>
2019-04-08 13:28:46 +1000

    Dashboards

--- 1-Initial-Setup/setup.sh
+++ 1-Initial-Setup/setup.sh
@@ -28,7 +28,7 @@ kctl() {
 #
 
 # B. MetalLB (MetalLB needs to be loaded first)
-kubectl apply -f --namespace=metallb-system https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
 
 # A. Core Configmaps
 kctl apply -f a-core-configmaps.yaml
