345b67dd3f51b4389e58b9fb476840e42a5e2e64
goldenpassport-admin <support@goldenpassport.com>
2019-04-20 20:52:20 +1000

    Initial

--- 3-External-Setup/run.sh
+++ 3-External-Setup/run.sh
@@ -28,8 +28,6 @@ sleep 5s
 # Helm / Consul
 #
 sudo helm repo update
-
-kubectl create -f rbac-config.yaml
 helm init --service-account tiller --tiller-image jessestuart/tiller --upgrade
 # Patch Helm to land on an ARM node because of the used image
 kubectl patch deployment tiller-deploy -n kube-system --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm64"}}}}}'
