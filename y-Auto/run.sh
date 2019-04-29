f9c0c1b62893f8f1f99b734989f0b60f775f52ef
goldenpassport-admin <support@goldenpassport.com>
2019-04-29 21:03:15 +1000

    Initial

--- y-Auto/run.sh
+++ y-Auto/run.sh
@@ -1,11 +1,15 @@
 sudo bash teardown-run.sh
-sleep 15s
+sleep 30s
 
 sudo bash 1-run.sh
-sleep 15s
+sleep 30s
+
+mkdir -p $HOME/.kube
+sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 sudo bash 2-run.sh
-sleep 15s
+sleep 30s
 
 sudo bash 3-run.sh
-sleep 15s
+sleep 30s
