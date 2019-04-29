3d3fac3420fe244f0b197f32c35f03dca4c45f2e
goldenpassport-admin <support@goldenpassport.com>
2019-04-29 10:25:21 +1000

    Initial

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-sudo bash z-Teardown/run.sh
+sudo bash ./z-Teardown/run.sh
 sleep 15s
 
-sudo bash 1-Install/run.sh
+sudo bash ./1-Install/run.sh
 sleep 15s
 
-sudo bash 2-Initial-Setup/run.sh
+sudo bash ./2-Initial-Setup/run.sh
 sleep 15s
 
-sudo bash 3-External-Setup/run.sh
+sudo bash ./3-External-Setup/run.sh
 sleep 15s
---
+++ test.sh
@@ -0,0 +1,2 @@
+sudo bash 2-Initial-Setup/run.sh
+sleep 15s
