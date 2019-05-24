d2ebfb23fe2ce97c89deb2f553c9ffd009350489
goldenpassport-admin <support@goldenpassport.com>
2019-05-24 18:34:29 +1000

    Initial

--- y-Auto/run.sh
+++ y-Auto/run.sh
@@ -4,12 +4,12 @@ sleep 30s
 sudo bash 1-run.sh
 sleep 30s
 
-mkdir -p $HOME/.kube
-sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
 sudo bash 2-run.sh
 sleep 30s
 
 sudo bash 3-run.sh
 sleep 30s
+
+echo "mkdir -p $HOME/.kube"
+echo "sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config"
+echo "sudo chown $(id -u):$(id -g) $HOME/.kube/config"
