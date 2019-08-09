3014829b398868dbfd0a62583954418d961c0b67
goldenpassport-admin <support@goldenpassport.com>
2019-08-09 21:08:16 +1000

    Initial

--- 1-Setup/ranchertest.sh
+++ 1-Setup/ranchertest.sh
@@ -172,6 +172,63 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
 sleep 5s
 
+set +o xtrace
+printf "\n\n#############################################"
+printf "\n### Enter dashboard admin account details ###"
+printf "\n#############################################\n\n"
+
+read -p "Enter username: " adminUsername
+printf "$adminUsername:`openssl passwd -apr1`\n" > ingress_auth.tmp
+kubectl create secret generic ingress-auth --from-file=ingress_auth.tmp -n kube-system 
+rm ingress_auth.tmp
+set -o xtrace
+
+#
+# Step 3 - Install Flannel, MetalLB and (Kube) Dashboards
+#
+
+# Flannel
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
+sleep 10s
+
+# MetalLB
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+kubectl apply -f metallb.yaml
+
+# Dashboard
+kctl apply -f dashboard.yaml
+# kubectl apply -f dashboard.yaml --namespace="kube-system"
+
+
+#
+# Step 4 - Install storageClass and Traefik
+#
+
+
+#
+# NFS-Storage
+#
+
+sudo rm -rf /mnt/storage/*
+kubectl apply -f nfs-storage.yaml
+##kubectl patch storageclass nfs-network -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch deployment nfs-client-provisioner -n nfs-storage --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm64"}}}}}'
+sleep 5s
+
+#
+# Tiller / Consul
+#
+
+set +o xtrace
+printf "\n\n##########################################"
+printf "\n### DNS provider API account details   ###"
+printf "\n##########################################\n\n"
+
+read -p "Enter key or username: " apiUsername
+read -p "Enter api secret: " apiSecret
+kubectl create secret generic traefik-external-provider -n kube-system --from-literal=key=$apiUsername --from-literal=secret=$apiSecret
+set -o xtrace
+
 # Tiller role
 kctl apply -f tiller.yaml
 sleep 15s
@@ -180,9 +237,66 @@ sudo helm init --service-account tiller --tiller-image jessestuart/tiller
 sleep 30s
 # Patch Helm to land on an ARM node because of the used image
 kubectl patch deployment tiller-deploy -n kube-system --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm64"}}}}}'
+sleep 15s
+
+# Consul
+{ 
+    sudo helm del --purge consul-traefik
+    echo "helm - consul-traefik deleted"
+} || { 
+    echo "helm - consul-traefik not found"
+}
+sleep 15s
+
+sudo helm install --name consul-traefik stable/consul --set ImageTag=1.4.4 --namespace kube-system
 sleep 30s
 
-sudo helm install rancher-latest/rancher \
+# Traefik - Internal
+#kctl apply -f traefik-internal.yaml
+# kubectl apply -f traefik-internal.yaml --namespace="kube-system"
+
+# Traefik - External
+#cat <<EOF | kubectl create -f -
+#EOF
+#sleep 30s
+
+#cat <<EOF | kubectl create -f -
+#EOF
+#sleep 30s
+
+kctl apply -f traefik-external.yaml
+#kubectl apply -f traefik-external.yaml --namespace="kube-system"
+kctl apply -f metrics-server.yaml
+kubectl apply -f prometheus.yaml
+#kubectl apply -f grafana.yaml
+
+#
+# Step 4 - Deploy NodeJs example app
+#
+
+#kubectl create namespace production
+#kubectl create namespace staging
+kubectl apply -f hello-world.yaml
+
+#
+# Step 5 - Final instructions
+#
+
+set +o xtrace
+
+printf "\n\n#####################################"
+printf "\n### Kubernetes join string        ###"
+printf "\n#####################################\n"
+kubeadm token create --print-join-command
+
+printf "\n#####################################"
+printf "\n### Enter below as a regular user ###"
+printf "\n#####################################\n"
+printf '\nmkdir -p $HOME/.kube'
+printf '\nsudo cp /etc/kubernetes/admin.conf $HOME/.kube/config'
+printf '\nsudo chown $(id -u):$(id -g) $HOME/.kube/config\n\n'
+
+helm install rancher-latest/rancher \
   --name rancher \
   --namespace cattle-system \
   --set hostname=rancher.goldenpassport.net \
