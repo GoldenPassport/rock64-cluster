83a8d0489f456aadfd3ec8d1efb656987b4857b2
goldenpassport-admin <support@goldenpassport.com>
2019-08-11 14:13:25 +1000

    Initial

--- 1-Setup/ranchertest.sh
+++ 1-Setup/ranchertest.sh
@@ -108,13 +108,9 @@ sudo usermod -aG docker rock
 }
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 sudo apt-get install -y kubelet kubeadm kubectl
-# apt-mark hold kubelet kubeadm kubectl
 
 # Init Kubernetes
 kubeadm config images pull
